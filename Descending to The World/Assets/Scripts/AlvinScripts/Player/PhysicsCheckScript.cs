5161ae50f1c4f500ec99a792282e28724c1cdcf2
Akiyama Alvin <139771544+NancoNya@users.noreply.github.com>
2025-03-31 19:09:09 +0800

    Revert "1"
    This reverts commit 84bc64814596837f519e3a34518a01332c727a78.

--- Descending to The World/Assets/Scripts/AlvinScripts/Player/PhysicsCheckScript.cs
+++ Descending to The World/Assets/Scripts/AlvinScripts/Player/PhysicsCheckScript.cs
@@ -81,12 +81,13 @@ public class PhysicsCheckScript : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Cloud"))
         {
-
+            isGround = false;
             isCloud = true;
         }
         else
         {
             isCloud = false;
+            isGround = true;
         }
     }
 }
